2c04d9d42e52e7d63d4568fc3aa3ea4decd1e048
John Joseph <47251026+JohnJoseph2007@users.noreply.github.com>
2020-12-12 16:55:31 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,15 @@
+class Box {
+    constructor(x, y, height, width){
+        this.body = Bodies.rectangle(x, y, height, width, {restitution : 0, isStatic : true});
+        World.add(world, this.body);
+        this.h = height;
+        this.w = width;
+    }
+
+    display() {
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("red")
+        rect(pos.x, pos.y, this.h, this.w);
+    }
+}
